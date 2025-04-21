439a2e7011ea0c34d74874ff0d179f6341ef9886
tlzkite <20758829+tlzkite@users.noreply.github.com>
2025-04-20 20:24:59 -0400

    Included language for GPL3 only.

--- oob_hunter.py
+++ oob_hunter.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
-# Bill Powell - 2025
+# Copyright (c) 2025 Bill Powell
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 **only**
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import sys
 from collections import defaultdict
